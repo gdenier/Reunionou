a277282c94910c5a28dbcde23a65fbbc83457b00
gdenier <gdenier@iut.univ-paris8.fr>
2018-07-06 11:35:51 +0200

    signin/signup fini

--- members/forms.py
+++ members/forms.py
@@ -1,10 +1,11 @@
 from django import forms
 
-class LoginForm(forms.Form):
-    username = forms.CharField(label="Identifiant", max_length=100, widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Nom d\'utilisateur'}))
-    password = forms.CharField(label="Mot de passe", max_length=100, widget=forms.PasswordInput)
-
 class SigninForm(forms.Form):
-    username = forms.CharField(label="Identifiant", max_length=100)
-    email = forms.CharField(label="Email", max_length=100, widget=forms.EmailInput)
-    password = forms.CharField(label="Mot de passe", max_length=100, widget=forms.PasswordInput)
+    username = forms.CharField(label="Identifiant", max_length=100, widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Nom d\'utilisateur'}))
+    password = forms.CharField(label="Mot de passe", max_length=100, widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Mot de passe'}))
+
+class SignupForm(forms.Form):
+    username = forms.CharField(label="Identifiant", max_length=100, widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Nom d\'utilisateur'}))
+    email = forms.CharField(label="Email", max_length=100, widget=forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'Adresse email'}))
+    password = forms.CharField(label="Mot de passe", max_length=100, widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Mot de passe'}))
+    password_conf = forms.CharField(label="Confirmation du mot de passe", max_length=100, widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Confirmation du mot de passe'}))
--- members/views.py
+++ members/views.py
@@ -4,15 +4,15 @@ from django.urls import reverse
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
 
-from .forms import LoginForm, SigninForm
+from .forms import SigninForm, SignupForm
 
 # Create your views here.
 
-def login_view(request):
+def signin_view(request):
     error = False
     
     if request.method == 'POST':
-        form = LoginForm(request.POST)
+        form = SigninForm(request.POST)
         if form.is_valid():
             user = authenticate(request, username=form.cleaned_data['username'], password=form.cleaned_data['password'])
             if user is not None:
@@ -21,9 +21,9 @@ def login_view(request):
             else:
                 error = True
     else:
-        form = LoginForm()
+        form = SigninForm()
     
-    return render(request, 'members/login.html', locals())
+    return render(request, 'members/signin.html', locals())
 
 def home_view(request):
     username = request.session.get('_auth_user_id')
@@ -31,19 +31,22 @@ def home_view(request):
 
 def logout_view(request):
     logout(request)
-    return HttpResponseRedirect(reverse('members:login'))
+    return HttpResponseRedirect(reverse('members:signin'))
 
-def signin_view(request):
+def signup_view(request):
     error = False
     if request.method == 'POST':
-        form = SigninForm(request.POST)
+        form = SignupForm(request.POST)
         if form.is_valid():
-            user = User.objects.create_user(form.cleaned_data['username'], form.cleaned_data['email'], form.cleaned_data['password'])
-            login(request, user)
-            return HttpResponseRedirect(reverse('members:home'))
+            if form.cleaned_data['password'] == form.cleaned_data['password_conf']:
+                user = User.objects.create_user(form.cleaned_data['username'], form.cleaned_data['email'], form.cleaned_data['password'])
+                login(request, user)
+                return HttpResponseRedirect(reverse('members:home'))
+            else:
+                error = True
         else:
             error = True
     else:
-        form = SigninForm()
+        form = SignupForm()
     
-    return render(request, 'members/signin.html', locals())
+    return render(request, 'members/signup.html', locals())
