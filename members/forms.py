74ffd16cb63042b0d78a35a0b4bc700088b3411b
edemots <eliot@demots.fr>
2018-07-14 12:53:44 +0200

    profil

--- members/forms.py
+++ members/forms.py
@@ -6,6 +6,8 @@ class SigninForm(forms.Form):
 
 class SignupForm(forms.Form):
     username = forms.CharField(label="Identifiant", max_length=100, widget=forms.TextInput(attrs={'class': 'input', 'placeholder': 'Nom d\'utilisateur'}))
+    # first_name= forms.CharField(label="Prénom", max_length=100, widget=forms.TextInput(attrs={'class': 'input', 'placeholder': 'Prénom'}))
+    # last_name = forms.CharField(label="Nom", max_length=100, widget=forms.TextInput(attrs={'class': 'input', 'placeholder': 'Nom'}))
     email = forms.CharField(label="Email", max_length=100, widget=forms.EmailInput(attrs={'class': 'input', 'placeholder': 'Adresse email'}))
     password = forms.CharField(label="Mot de passe", max_length=100, widget=forms.PasswordInput(attrs={'class': 'input', 'placeholder': 'Mot de passe'}))
     password_conf = forms.CharField(label="Confirmation du mot de passe", max_length=100, widget=forms.PasswordInput(attrs={'class': 'input', 'placeholder': 'Confirmation du mot de passe'}))
